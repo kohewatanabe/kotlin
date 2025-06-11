784d140a7cae641371de32bcc3150c11d8e62b3e
kohewatanabe <kohei.watanabe.1203@gmail.com>
2025-06-12 07:54:47 +0900

    2

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,10 @@
 //TIP コードを<b>実行</b>するには、<shortcut actionId="Run"/> を押すか
 // ガターの <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
 fun main() {
-    val name = "Kotlin"
-    //TIP ハイライトされたテキストにキャレットがある状態で <shortcut actionId="ShowIntentionActions"/> を押すと
-    // IntelliJ IDEA によるその修正案を確認できます。
-    println("Hello, " + name + "!")
-
-    for (i in 1..5) {
-        //TIP <shortcut actionId="Debug"/> を押してコードのデバッグを開始します。<icon src="AllIcons.Debugger.Db_set_breakpoint"/> ブレークポイントを 1 つ設定しましたが、
-        // <shortcut actionId="ToggleLineBreakpoint"/> を押すといつでも他のブレークポイントを追加できます。
-        println("i = $i")
-    }
+    var n: Int = 10
+    var m = 1.5//型推論(コンパイラが値から変数の型を推論して勝手に付けてくれる)
+    var s: Any//(変数に任意の型を持たせたい場合はAny型を指定する)
+    s = 10//Kotlinでは全てのデータはオブジェクトであり、それらは全てAny型を継承している。
+    s = "foo"
+    print(s)
 }
